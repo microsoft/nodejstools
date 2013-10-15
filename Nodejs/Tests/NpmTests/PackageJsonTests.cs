df16a54bfeca1ed70a9859986f5676de6ddfa292
Bart Read <bart.read@red-gate.com>
2013-10-15 18:41:08 +0100

    Added first (failing) test for reading package.json - just reads name and version, which are perhaps the two most important properties, obviously discounting dependencies

---
+++ Nodejs/Tests/NpmTests/PackageJsonTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+
+namespace NpmTests
+{
+    [TestClass]
+    public class PackageJsonTests
+    {
+        [TestMethod]
+        public void TestReadNameAndVersion()
+        {
+            const string pkgJsonStr = @"{
+    ""name"": ""TestPkg"",
+    ""version"": ""0.1.0""
+}";
+
+            var source = new MockPackageJsonSource( pkgJsonStr );
+            IPackageJson pkgJson = PackageJsonFactory.Create( source );
+
+            dynamic json = JsonConvert.DeserializeObject( pkgJsonStr );
+
+            Assert.AreEqual( json.name, pkgJson.Name, "Names should match." );
+            Assert.AreEqual( json.version, pkgJson.Version.ToString() );
+        }
+    }
+}
