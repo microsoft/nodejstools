78dad7cf62ae81fa73c021516f662b266934e505
Bart Read <bart.read@red-gate.com>
2013-10-21 17:23:34 +0100

    Added ability to access pre-release and build metadata information in semantic version, which can be used by package authors. Currently the only test supplies a 'normal' X.Y.Z version so I need to add some more tests to check the parsing works OK. It's not meant to be bulletproof - just provide a way for people to work on code that has these values set in the version and, ultimately, allow authors to set them via VS.

--- Nodejs/Product/Npm/SemverVersion.cs
+++ Nodejs/Product/Npm/SemverVersion.cs
@@ -18,17 +18,62 @@ namespace Microsoft.NodejsTools.Npm
         private int m_Minor;
         private int m_Patch;
 
+        //  N.B. Both of these are series of dot separated identifiers, but since we don't really particularly
+        //  care about them at the moment, can defer comparisons to semver, and won't need to do anything beyond
+        //  let the user specify a value, I've just implemented them as strings.
+        private string m_PreReleaseVersion;
+        private string m_BuildMetadata;
+
         public SemverVersion( string versionString )
         {
-            var parts = versionString.Split( '.' );
-            m_Major = int.Parse( parts[ 0 ] );
-            m_Minor = int.Parse( parts[ 1 ] );
-            m_Patch = int.Parse( parts[ 2 ] );
+            var parts       = versionString.Split( '-' );
+            var normalParts = parts[ 0 ].Split( '.' );
+
+            m_Major = int.Parse( normalParts[0] );
+            m_Minor = int.Parse( normalParts[1] );
+            m_Patch = int.Parse( normalParts[2] );
+
+            m_PreReleaseVersion = null;
+            m_BuildMetadata = null;
+
+            if (parts.Length > 1)
+            {
+                if ( parts.Length > 2 )
+                {
+                    throw new ArgumentException(
+                        string.Format(
+                            "Invalid semantic version: '{0}'. Cannot have more than one pre-release version in a semantic version.",
+                            versionString ),
+                        "versionString" );
+                }
+
+                parts = parts[ 1 ].Split( '+' );
+                m_PreReleaseVersion = parts[ 0 ];
+
+                if ( parts.Length > 1 )
+                {
+                    if ( parts.Length > 2 )
+                    {
+                        throw new ArgumentException(
+                            string.Format(
+                                "Invalid semantic version: '{0}'. Cannot have more than one build metadata chunk in a semantic version.",
+                                versionString ),
+                            "versionString" );
+                    }
+
+                    m_BuildMetadata = parts[ 1 ];
+                }
+            }
         }
 
         public int Major { get { return m_Major; } }
         public int Minor { get { return m_Minor; } }
         public int Patch { get { return m_Patch; } }
+
+        public bool HasPreReleaseVersion { get { return ! string.IsNullOrEmpty( PreReleaseVersion ); } }
+        public string PreReleaseVersion { get { return m_PreReleaseVersion; } }
+        public bool HasBuildMetadata { get { return ! string.IsNullOrEmpty( BuildMetadata ); } }
+        public string BuildMetadata { get { return m_BuildMetadata; } }
 
         public override string ToString()
         {
---
+++ Nodejs/Tests/NpmTests/SemverVersionTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.NodejsTools.Npm;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NpmTests
+{
+
+    /// <summary>
+    /// Tests for semantic version parsing/output. Only exists because users may
+    /// need to specify a pre-release version or build metadata when authoring their
+    /// own packages.
+    /// </summary>
+    [TestClass]
+    public class SemverVersionTests
+    {
+        [TestMethod]
+        public void TestBasicMajorMinorPatchVersion()
+        {
+            SemverVersion   version = new SemverVersion( "1.2.3" );
+            Assert.AreEqual( 1, version.Major );
+            Assert.AreEqual( 2, version.Minor, "Mismatched minor version." );
+            Assert.AreEqual( 3, version.Patch, "Mismatched patch version." );
+            Assert.AreEqual( false, version.HasPreReleaseVersion, "Should not have pre-release version info." );
+            Assert.AreEqual( null, version.PreReleaseVersion, "Pre-release version info should be null." );
+            Assert.AreEqual( false, version.HasBuildMetadata, "Should not have build metadata." );
+            Assert.AreEqual( null, version.BuildMetadata, "Build metadata should be null or empty.");
+        }
+    }
+}
