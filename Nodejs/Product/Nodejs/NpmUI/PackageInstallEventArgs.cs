e6422ba18ac52549e77ed8b6b0b0ff5b198f7851
Bart Read <bart.read@red-gate.com>
2013-11-07 19:33:17 +0000

    Added event to be fired when package install requested by user.

---
+++ Nodejs/Product/Nodejs/NpmUI/PackageInstallEventArgs.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.NodejsTools.Npm;
+
+namespace Microsoft.NodejsTools.NpmUI
+{
+    internal class PackageInstallEventArgs : EventArgs
+    {
+
+        public PackageInstallEventArgs(
+            string name,
+            string version,
+            DependencyType depType )
+        {
+            Name = name;
+            Version = version;
+            DependencyType = depType;
+        }
+
+        public string Name { get; private set; }
+        public string Version { get; private set; }
+        public DependencyType DependencyType { get; private set; }
+    }
+}
