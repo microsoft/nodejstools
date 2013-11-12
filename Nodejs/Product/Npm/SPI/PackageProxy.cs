8c39bf6337a0191e7cd3a08b5f55683f661a535c
Bart Read <bart.read@red-gate.com>
2013-11-12 23:30:19 +0000

    Added Microsoft copyright and licensing notice to top of all source files.

--- Nodejs/Product/Npm/SPI/PackageProxy.cs
+++ Nodejs/Product/Npm/SPI/PackageProxy.cs
@@ -1,4 +1,19 @@
-﻿namespace Microsoft.NodejsTools.Npm.SPI{
+﻿/* ****************************************************************************
+ *
+ * Copyright (c) Microsoft Corporation. 
+ *
+ * This source code is subject to terms and conditions of the Apache License, Version 2.0. A 
+ * copy of the license can be found in the License.html file at the root of this distribution. If 
+ * you cannot locate the Apache License, Version 2.0, please send an email to 
+ * vspython@microsoft.com. By using this source code in any fashion, you are agreeing to be bound 
+ * by the terms of the Apache License, Version 2.0.
+ *
+ * You must not remove this notice, or any other, from this software.
+ *
+ * ***************************************************************************/
+
+namespace Microsoft.NodejsTools.Npm.SPI
+{
     internal class PackageProxy : IPackage{
         public INodeModules Modules { get; internal set; }
         public IPackageJson PackageJson { get; internal set; }
