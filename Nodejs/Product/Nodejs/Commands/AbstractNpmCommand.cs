5808a0b92a76423f602be8e04bcc0224e9bdb0ad
Bart Read <bart.read@red-gate.com>
2013-11-13 00:34:47 +0000

    Reformatted code and added Microsoft copyright notice to top of files I'd forgotten earlier.

--- Nodejs/Product/Nodejs/Commands/AbstractNpmCommand.cs
+++ Nodejs/Product/Nodejs/Commands/AbstractNpmCommand.cs
@@ -1,28 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿/* ****************************************************************************
+ *
+ * Copyright (c) Microsoft Corporation. 
+ *
+ * This source code is subject to terms and conditions of the Apache License, Version 2.0. A 
+ * copy of the license can be found in the License.html file at the root of this distribution. If 
+ * you cannot locate the Apache License, Version 2.0, please send an email to 
+ * vspython@microsoft.com. By using this source code in any fashion, you are agreeing to be bound 
+ * by the terms of the Apache License, Version 2.0.
+ *
+ * You must not remove this notice, or any other, from this software.
+ *
+ * ***************************************************************************/
+
+using System;
 using Microsoft.NodejsTools.Project;
 using Microsoft.VisualStudioTools;
 
-namespace Microsoft.NodejsTools.Commands
-{
-    abstract class AbstractNpmCommand : Command
-    {
+namespace Microsoft.NodejsTools.Commands{
+    internal abstract class AbstractNpmCommand : Command{
         public NodeModulesNode ModulesNode { get; set; }
 
-        public override EventHandler BeforeQueryStatus
-        {
+        public override EventHandler BeforeQueryStatus{
             get { return BeforeQueryStatusImpl; }
         }
 
-        private void BeforeQueryStatusImpl( object source, EventArgs args )
-        {
+        private void BeforeQueryStatusImpl(object source, EventArgs args){
             var node = ModulesNode;
-            if ( null != node )
-            {
-                node.BeforeQueryStatus( source, args );
+            if (null != node){
+                node.BeforeQueryStatus(source, args);
             }
         }
     }
--- Nodejs/Product/Nodejs/Commands/UninstallModulesCommand.cs
+++ Nodejs/Product/Nodejs/Commands/UninstallModulesCommand.cs
@@ -1,25 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.VisualStudioTools;
+﻿/* ****************************************************************************
+ *
+ * Copyright (c) Microsoft Corporation. 
+ *
+ * This source code is subject to terms and conditions of the Apache License, Version 2.0. A 
+ * copy of the license can be found in the License.html file at the root of this distribution. If 
+ * you cannot locate the Apache License, Version 2.0, please send an email to 
+ * vspython@microsoft.com. By using this source code in any fashion, you are agreeing to be bound 
+ * by the terms of the Apache License, Version 2.0.
+ *
+ * You must not remove this notice, or any other, from this software.
+ *
+ * ***************************************************************************/
 
-namespace Microsoft.NodejsTools.Commands
-{
-    class UninstallModulesCommand : AbstractNpmCommand
-    {
-        public override void DoCommand( object sender, EventArgs args )
-        {
+using System;
+
+namespace Microsoft.NodejsTools.Commands{
+    internal class UninstallModulesCommand : AbstractNpmCommand{
+        public override void DoCommand(object sender, EventArgs args){
             var node = ModulesNode;
-            if ( null != node )
-            {
+            if (null != node){
                 node.UninstallModules();
             }
         }
 
-        public override int CommandId
-        {
+        public override int CommandId{
             get { return PkgCmdId.cmdidNpmUninstallModule; }
         }
     }
