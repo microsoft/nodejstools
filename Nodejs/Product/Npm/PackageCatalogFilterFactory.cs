0ac6b3d9fb00b53575c529bc565aaff1e26a35ae
Bart Read <bart.read@me.com>
2014-02-15 21:55:12 +0000

    Added interface for filtering packages by string and regex, and factory for creating concrete instances. Also committing changes to projects. Forgot to commit last time when I moved that file. Relates to item 495.

---
+++ Nodejs/Product/Npm/PackageCatalogFilterFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.NodejsTools.Npm {
+    public static class PackageCatalogFilterFactory {
+        public static IPackageCatalogFilter Create(IPackageCatalog catalog) {
+            throw new NotImplementedException();
+        }
+    }
+}
