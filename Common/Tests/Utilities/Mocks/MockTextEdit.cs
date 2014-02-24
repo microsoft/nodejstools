4ec6000ca5fb23a58e4c9c1f9eb774b56eae5841
RickWinter <rick.winter@microsoft.com>
2014-02-24 11:16:10 -0800

    ReApply the support for non-overlapping edits

--- Common/Tests/Utilities/Mocks/MockTextEdit.cs
+++ Common/Tests/Utilities/Mocks/MockTextEdit.cs
@@ -15,6 +15,7 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 using Microsoft.VisualStudio.Text;
 
 namespace TestUtilities.Mocks {
@@ -78,28 +79,37 @@ namespace TestUtilities.Mocks {
         }
 
         public ITextSnapshot Apply() {
-            // this works for non-overlapping edits...
             StringBuilder text = new StringBuilder(_snapshot.GetText());
+            var deletes = new NormalizedSnapshotSpanCollection(
+                _snapshot,
+                _edits.Where(edit => edit is DeletionEdit)
+                .Select(edit =>
+                    new Span(
+                        ((DeletionEdit)edit).Position,
+                        ((DeletionEdit)edit).Length
+                    )
+                )
+            );
+
+            // apply the deletes
+            for (int i = deletes.Count - 1; i >= 0; i--) {
+                text.Remove(deletes[i].Start, deletes[i].Length);
+            }
+
+            // now apply the inserts
+            int curDelete = 0, adjust = 0;
+            foreach (InsertionEdit insert in _edits.Where(edit => edit is InsertionEdit)) {
+                while (curDelete < deletes.Count && deletes[curDelete].Start < insert.Position) {
+                    adjust -= deletes[curDelete++].Length;
+                }
+
+                text.Insert(insert.Position + adjust, insert.Text);
+                adjust += insert.Text.Length;
+            }
+
             List<MockTextChange> changes = new List<MockTextChange>();
             for (int i = 0; i < _edits.Count; i++) {
                 var curEdit = _edits[i];
-
-                int adjust = 0;
-                for (int j = 0; j < i; j++) {
-                    var compEdit = _edits[j];
-                    DeletionEdit del = compEdit as DeletionEdit;
-                    if (del != null) {
-                        if ((compEdit.Position) < curEdit.Position) {
-                            adjust -= del.Length;
-                        }
-                    } else {
-                        if ((compEdit.Position) <= curEdit.Position) {
-                            adjust += ((InsertionEdit)compEdit).Text.Length;
-                        }
-                    }
-                }
-
-
                 InsertionEdit insert = curEdit as InsertionEdit;
                 if (insert != null) {
                     changes.Add(
@@ -113,7 +123,6 @@ namespace TestUtilities.Mocks {
                             insert.Text
                         )
                     );
-
                 } else {
                     DeletionEdit delete = curEdit as DeletionEdit;
                     changes.Add(
@@ -127,7 +136,6 @@ namespace TestUtilities.Mocks {
                             ""
                         )
                     );
-                    text.Remove(delete.Position + adjust, delete.Length);
                 }
             }
 
