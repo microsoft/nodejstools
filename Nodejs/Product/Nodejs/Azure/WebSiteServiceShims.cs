73ef93522651579329c383225a3b68c8ecfcf23b
pminaev <pminaev@microsoft.com>
2014-03-11 14:26:09 -0700

    #748 Attach Debugger command not visible in Server Explorer with VS Update 2
    The contracts assembly for Azure websites VS API has changed in SDK 2.3 (which is also in VS Update 2) in a way that breaks existing clients, forcing us to support one of the other. This change updates our code so that it uses the new interfaces. The contract assembly in question is going to be stable from 2.3 RC onwards. Reflection is used to access the interfaces to avoid taking a compile-time dependency on private builds from the 2.3 branch with updated interfaces.
    
    This effectively disables the Attach debugger (node.js) command in Server Explorer for earlier SDK versions (2.3 CTP 2 and below). The remote attach functionality will still be available there via Process -> Attach to Server, by directly entering a ws:// URL.

---
+++ Nodejs/Product/Nodejs/Azure/WebSiteServiceShims.cs
@@ -0,0 +1,192 @@
+﻿/* ****************************************************************************
+ *
+ * Copyright (c) Microsoft Corporation. 
+ *
+ * This source code is subject to terms and conditions of the Apache License, Version 2.0. A 
+ * copy of the license can be found in the License.html file at the root of this distribution. If 
+ * you cannot locate the Apache License, Version 2.0, please send an email to 
+ * vspython@microsoft.com. By using this source code in any fashion, you are agreeing to be bound 
+ * by the terms of the Apache License, Version 2.0.
+ *
+ * You must not remove this notice, or any other, from this software.
+ *
+ * ***************************************************************************/
+
+
+// This file contains Reflectio-based shims for interfaces from the Microsoft.VisualStudio.Web.WindowsAzure.Contracts.
+// They are used for the time being because the interfaces in question are only going to be stabilized in Visual Studio 2013 Update 2,
+// and until then using them directly requires private builds of Azure Tools to build against. Using Reflection avoids that dependency.
+// The interfaces correspond directly to those in the contracts assembly, but only those members that are actually used by our code are
+// exposed and shimmed.
+//
+// TODO: get rid of the shims and use the contracts assembly directly once Update 2 RTM is out.
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.WindowsAzure.Authentication;
+
+namespace Microsoft.VisualStudio.Web.WindowsAzure.Contracts.Shims {
+    internal class ContractShim<T> {
+        private static readonly Assembly contractAssembly = Assembly.Load("Microsoft.VisualStudio.Web.WindowsAzure.Contracts, Version=2.3.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a");
+        private static readonly Type _interface;
+        private readonly object _impl;
+
+        static ContractShim() {
+            string shimName = typeof(T).FullName;
+            Debug.Assert(shimName.StartsWith("Microsoft.VisualStudio.Web.WindowsAzure.Contracts.Shims."));
+            string realName = shimName.Replace(".Shims.", ".");
+            _interface = contractAssembly.GetType(realName, throwOnError: true);
+        }
+
+        protected ContractShim(object impl) {
+            _impl = impl;
+        }
+
+        public static bool CanShim(object impl) {
+            return impl != null && impl.GetType().GetInterfaces().Contains(_interface);
+        }
+
+        private object InvokeByName(string name, object[] args) {
+            var method =
+                new[] { _interface }
+                .Concat(_interface.GetInterfaces())
+                .Select(t => t.GetMethod(name))
+                .Where(m => m != null)
+                .FirstOrDefault();
+            if (method == null) {
+                throw new MissingMethodException(_interface.FullName, name);
+            }
+            return method.Invoke(_impl, args);
+        }
+
+        protected object Invoke([CallerMemberName] string name = null) {
+            return InvokeByName(name, null);
+        }
+
+        protected object Invoke(object arg1, [CallerMemberName] string name = null) {
+            return InvokeByName(name, new[] { arg1 });
+        }
+
+        protected object Invoke(object arg1, object arg2, [CallerMemberName] string name = null) {
+            return InvokeByName(name, new[] { arg1, arg2 });
+        }
+
+        protected object Get([CallerMemberName] string name = null) {
+            var prop =
+                new[] { _interface }
+                .Concat(_interface.GetInterfaces())
+                .Select(t => t.GetProperty(name))
+                .Where(p => p != null)
+                .FirstOrDefault();
+            if (prop == null) {
+                throw new MissingMemberException(_interface.FullName, name);
+            }
+            return prop.GetValue(_impl);
+        }
+    }
+
+    [Guid("D756EDAE-0998-42AA-AA02-6B4FD029E731")]
+    internal interface IVsAzureServices {
+        IAzureWebSitesService GetAzureWebSitesService();
+    }
+
+    internal class VsAzureServicesShim : ContractShim<IVsAzureServices>, IVsAzureServices {
+        public VsAzureServicesShim(object impl)
+            : base(impl) {
+        }
+
+        public IAzureWebSitesService GetAzureWebSitesService() {
+            return new AzureWebSitesServiceShim(Invoke());
+        }
+    }
+
+    internal interface IAzureService {
+        Task<List<IAzureSubscription>> GetSubscriptionsAsync();
+    }
+
+    internal interface IAzureWebSitesService : IAzureService {
+    }
+
+    internal class AzureWebSitesServiceShim : ContractShim<IAzureWebSitesService>, IAzureWebSitesService {
+        public AzureWebSitesServiceShim(object impl)
+            : base(impl) {
+        }
+        
+        public Task<List<IAzureSubscription>> GetSubscriptionsAsync() {
+            return ((Task)Invoke()).ContinueWith(task =>
+                ((IEnumerable<object>)((dynamic)task).Result)
+                .Select(item => (IAzureSubscription)new AzureSubscriptionShim(item))
+                .ToList()
+            );
+        }
+    }
+
+    internal interface IAzureSubscription {
+        string SubscriptionId { get; }
+        Uri ServiceManagementEndpointUri { get; }
+        IAzureSubscriptionContext AzureCredentials { get; }
+        Task<List<IAzureResource>> GetResourcesAsync(bool refresh);
+    }
+
+    internal class AzureSubscriptionShim : ContractShim<IAzureSubscription>, IAzureSubscription {
+        public AzureSubscriptionShim(object impl)
+            : base(impl) {
+        }
+
+        public string SubscriptionId {
+            get { return (string)Get(); }
+        }
+
+        public Uri ServiceManagementEndpointUri {
+            get { return (Uri)Get(); }
+        }
+
+        public IAzureSubscriptionContext AzureCredentials {
+            get { return (IAzureSubscriptionContext)Get(); }
+        }
+
+        public Task<List<IAzureResource>> GetResourcesAsync(bool refresh) {
+            // The caller will only use websites from this list, and we don't
+            // want to shim other resource types, so filter them out.
+            return ((Task)Invoke(refresh)).ContinueWith(task =>
+                ((IEnumerable<object>)((dynamic)task).Result)
+                .Where(item => AzureWebSiteShim.CanShim(item))
+                .Select(item => (IAzureResource)new AzureWebSiteShim(item))
+                .ToList()
+            );
+        }
+    }
+
+    internal interface IAzureResource {
+        string Name { get; }
+    }
+
+    internal interface IAzureWebSite : IAzureResource {
+        string WebSpace { get; }
+        string BrowseURL { get; }
+    }
+
+    internal class AzureWebSiteShim : ContractShim<IAzureWebSite>, IAzureWebSite {
+        public AzureWebSiteShim(object impl)
+            : base(impl) {
+        }
+
+        public string WebSpace {
+            get { return (string)Get(); }
+        }
+
+        public string BrowseURL {
+            get { return (string)Get(); }
+        }
+
+        public string Name {
+            get { return (string)Get(); }
+        }
+    }
+}
