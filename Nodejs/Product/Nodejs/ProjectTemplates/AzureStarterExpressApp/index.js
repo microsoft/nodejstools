ee2f6f845053f509e9c082ab75ca7e2883978bc6
huguesv <huvalo@microsoft.com>
2014-07-31 14:51:56 -0700

    Add starter templates for Express, based on PTVS templates. Fix some of the existing templates, which had fixed name/project guid.

---
+++ Nodejs/Product/Nodejs/ProjectTemplates/AzureStarterExpressApp/index.js
@@ -0,0 +1,16 @@
+﻿
+/*
+ * GET home page.
+ */
+
+exports.index = function (req, res) {
+    res.render('index', { title: 'Express', year: new Date().getFullYear() });
+};
+
+exports.about = function (req, res) {
+    res.render('about', { title: 'About', year: new Date().getFullYear(), message: 'Your application description page.' });
+};
+
+exports.contact = function (req, res) {
+    res.render('contact', { title: 'Contact', year: new Date().getFullYear(), message: 'Your contact page.' });
+};
