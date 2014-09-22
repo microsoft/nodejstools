fe8a171115aaf768457941087ecbb7076290fa61
RickWinter <rick.winter@microsoft.com>
2014-09-22 11:04:33 -0700

    #1345 - Remove test parameter from unit test item template

--- Nodejs/Product/Nodejs/Templates/Files/TypeScriptUnitTest/UnitTest.ts
+++ Nodejs/Product/Nodejs/Templates/Files/TypeScriptUnitTest/UnitTest.ts
@@ -1,10 +1,10 @@
 ﻿import assert = require('assert');
 
-export function Test1(test) {
+export function Test1() {
     assert.ok(true, "This shouldn't fail");
 }
 
-export function Test2(test) {
+export function Test2() {
     assert.ok(1 === 1, "This shouldn't fail");
     assert.ok(false, "This should fail");
 }
--- Nodejs/Product/Nodejs/Templates/Files/UnitTest/UnitTest.js
+++ Nodejs/Product/Nodejs/Templates/Files/UnitTest/UnitTest.js
@@ -1,10 +1,10 @@
 ﻿var assert = require('assert');
 
-exports['Test 1'] = function (test) {
+exports['Test 1'] = function () {
     assert.ok(true, "This shouldn't fail");
 }
 
-exports['Test 2'] = function (test) {
+exports['Test 2'] = function () {
     assert.ok(1 === 1, "This shouldn't fail");
     assert.ok(false, "This should fail");
 }
