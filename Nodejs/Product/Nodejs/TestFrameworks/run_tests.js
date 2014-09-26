14d005effa2cc1408cbabb19d9f025108734dd1a
RickWinter <rick.winter@microsoft.com>
2014-09-26 10:13:11 -0700

    #1391 - ExportRunner does not fail any tests #1390 - assert.throws does not fail in Test Explorer

--- Nodejs/Product/Nodejs/TestFrameworks/run_tests.js
+++ Nodejs/Product/Nodejs/TestFrameworks/run_tests.js
@@ -3,11 +3,12 @@ try {
     framework = require('./' + process.argv[2] + '/' + process.argv[2] + '.js');
 } catch (exception) {
     console.log("NTVS_ERROR:Failed to load TestFramework (" + process.argv[2] + "), " + exception);
-    exit;
+    process.exit(1);
 }
 try {
     framework.run_tests(process.argv[3], process.argv[4], process.argv[5], process.argv[6]);
 } catch (exception) {
     console.log("NTVS_ERROR:TestFramework (" + process.argv[2] + ") threw an exception processing (" + process.argv[3] + "), " + exception);
+    process.exit(1);
 }
-
+process.exit(0);
