7ea8a49cf6b3e5fc914b790efe8c08c92dc8730e
sanyamc-msft <sanyamc@microsoft.com>
2015-08-27 14:40:28 -0700

    Updating with latest changes from master

--- Nodejs/Prerequisites/generate.js
+++ Nodejs/Prerequisites/generate.js
@@ -41,10 +41,9 @@ var assemblies = [
     'Microsoft.NodejsTools.PressAnyKey',
     'Microsoft.NodejsTools.Telemetry.11.0',
     'Microsoft.NodejsTools.Telemetry.12.0',
-    'Microsoft.NodejsTools.Telemetry.14.0'
+    'Microsoft.NodejsTools.Telemetry.14.0',
     'Microsoft.NodejsTools.InstallerTelemetryAction.CA',
-    'Microsoft.NodejsTools.InstallerTelemetryAction',
-    'Microsoft.NodejsTools.VsLogger'
+    'Microsoft.NodejsTools.InstallerTelemetryAction'
 ].sort();
 
 var files = [
