9baf42fcedc5acdad767dba1296475ba242d4925
Tien Nguyen <tihoanh@microsoft.com>
2015-09-04 17:01:45 -0700

    Address feedback.

--- Nodejs/Tests/Core/Mocks/MockTextBuffer.cs
+++ Nodejs/Tests/Core/Mocks/MockTextBuffer.cs
@@ -1,8 +1,20 @@
-﻿using Microsoft.NodejsTools;
+﻿using System.IO;
+using Microsoft.NodejsTools;
 
 namespace NodejsTests.Mocks {
     class MockTextBuffer : TestUtilities.Mocks.MockTextBuffer {
-		public MockTextBuffer(string content) : base(content: content, contentType: NodejsConstants.Nodejs, filename: "file.js") {
+		public MockTextBuffer(string content) : base(content: content, contentType: NodejsConstants.Nodejs) {
+        }
+
+        private static string GetRandomFileNameIfNull(string filename) {
+            if (filename == null) {
+                filename = Path.Combine(TestUtilities.TestData.GetTempPath(), Path.GetRandomFileName(), "file.js");
+            }
+            return filename;
+        }
+
+        public MockTextBuffer(string content, string contentType, string filename = null) : base(content: content,
+            contentType: contentType, filename: MockTextBuffer.GetRandomFileNameIfNull(filename)) {
         }
     }
 }
