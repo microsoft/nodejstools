a7ff928dfeb95f72411910c5f4e1a02fa7edaa9a
Matt Bierner <matb@microsoft.com>
2016-02-25 13:27:21 -0800

    Fix spelling and reorder interface
    Small fix to spelling and reordering to put private methods last based on review comments.

--- Nodejs/Product/ProjectWizard/NodejsPackageParametersExtension.cs
+++ Nodejs/Product/ProjectWizard/NodejsPackageParametersExtension.cs
@@ -22,25 +22,6 @@ using Microsoft.VisualStudio.TemplateWizard;
 
 namespace Microsoft.NodejsTools.ProjectWizard {
     class NodejsPackageParametersExtension : IWizard {
-        private const int NpmPackageNameMaxLength = 214;
-
-        /// <summary>
-        /// Normalize a project name to be a valid Npm package name.
-        /// </summary>
-        /// <param name="projectName">Name of a VS project.</param>
-        private static string NormalizeNpmPackageName(string projectName) {
-            // Remove all leading url-invalid, underscore, and period characters
-            var npmProjectNameTransform = Regex.Replace(projectName, "^[^a-zA-Z0-9-~]*", string.Empty);
-
-            // Replace all invalid characters with a dash
-            npmProjectNameTransform = Regex.Replace(npmProjectNameTransform, "[^a-zA-Z0-9-_~.]", "-");
-
-            // insert hypens between camelcased sections.
-            npmProjectNameTransform = Regex.Replace(npmProjectNameTransform, "([a-z0-9])([A-Z])", "$1-$2").ToLowerInvariant();
-
-            return npmProjectNameTransform.Substring(0, Math.Min(npmProjectNameTransform.Length, NpmPackageNameMaxLength));
-        }
-
         public void RunStarted(object automationObject, Dictionary<string, string> replacementsDictionary, WizardRunKind runKind, object[] customParams) {
             var projectName = replacementsDictionary["$projectname$"];
             replacementsDictionary.Add("$npmsafeprojectname$", NormalizeNpmPackageName(projectName));
@@ -64,6 +45,25 @@ namespace Microsoft.NodejsTools.ProjectWizard {
 
         public void RunFinished() {
             return;
+        }
+
+        private const int NpmPackageNameMaxLength = 214;
+
+        /// <summary>
+        /// Normalize a project name to be a valid Npm package name: https://docs.npmjs.com/files/package.json#name
+        /// </summary>
+        /// <param name="projectName">Name of a VS project.</param>
+        private static string NormalizeNpmPackageName(string projectName) {
+            // Remove all leading url-invalid, underscore, and period characters
+            var npmProjectNameTransform = Regex.Replace(projectName, "^[^a-zA-Z0-9-~]*", string.Empty);
+
+            // Replace all invalid characters with a dash
+            npmProjectNameTransform = Regex.Replace(npmProjectNameTransform, "[^a-zA-Z0-9-_~.]", "-");
+
+            // Insert hyphens between camelcased sections.
+            npmProjectNameTransform = Regex.Replace(npmProjectNameTransform, "([a-z0-9])([A-Z])", "$1-$2").ToLowerInvariant();
+
+            return npmProjectNameTransform.Substring(0, Math.Min(npmProjectNameTransform.Length, NpmPackageNameMaxLength));
         }
     }
 }
