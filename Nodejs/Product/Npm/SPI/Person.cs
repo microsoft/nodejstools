4d015187a6618798f59cd5e9e5b46a110e58badd
Matt Bierner <matb@microsoft.com>
2016-03-01 16:47:09 -0800

    Move Npm::Person to use a factory instead of constructor.
    This class is just a simple structure and its constructors should not be doing all this parsing.

--- Nodejs/Product/Npm/SPI/Person.cs
+++ Nodejs/Product/Npm/SPI/Person.cs
@@ -29,58 +29,43 @@ namespace Microsoft.NodejsTools.Npm.SPI {
 
         // We cannot rely on the ordering of any of these fields,
         // so we should match them separately.
-        private static readonly Regex RegexPerson = new Regex(
-        "\"name\":\\s*\"(?<name>[^<]+?)\"" +
-        "|" +
-        "\"email\":\\s*\"(?<email>[^<]+?)\"" +
-        "|" +
-        "\"url\":\\s*\"(?<url>[^<]+?)\"",
-        RegexOptions.Singleline);
-    
+        private static readonly Regex ObjectPersonRegex = new Regex(
+            "\"name\":\\s*\"(?<name>[^<]+?)\"" +
+            "|" +
+            "\"email\":\\s*\"(?<email>[^<]+?)\"" +
+            "|" +
+            "\"url\":\\s*\"(?<url>[^<]+?)\"",
+            RegexOptions.Singleline);
+
+        private static readonly Regex StringPersonRegex = new Regex(
+            "^\"([^\"]+)\"$",
+            RegexOptions.Singleline);
+
         [JsonConstructor]
         private Person() {
             // Enables Json deserialization
         }
 
-        public Person(string source) {
-            InitFromString(source);
+        private Person(string name, string email = "", string url = "") {
+            Name = name;
+            Email = email;
+            Url = url;
         }
 
-        private void InitFromString(string source) {
-            if (source == null) {
-                Name = string.Empty;
-                return;
-            }
+        public static Person CreateFromJsonSource(string source) {
+            if (source == null)
+                return new Person(string.Empty);
+            
 
-            // We parse using a regex because JObject.Parse throws exceptions for malformatted json,
-            // and simply handling them causes performance issues.
-            var matches = RegexPerson.Matches(source);
-            if (matches.Count >= 1) {
-                foreach (Match match in matches) {
-                    var group = match.Groups["name"];
-                    if (group.Success) {
-                        Name = group.Value;
-                        continue;
-                    }
+            var objectPerson = TryCreatePersonFromObject(source);
+            if (objectPerson != null)
+                return objectPerson;
 
-                    group = match.Groups["email"];
-                    if (group.Success) {
-                        Email = group.Value;
-                        continue;
-                    }
-
-                    group = match.Groups["url"];
-                    if (group.Success) {
-                        Url = group.Value;
-                        continue;
-                    }
-                }
-            } else {
-                Name = source;
-            }
-
+            var stringPerson = TryCreatePersonFromString(source);
+            if (stringPerson != null)
+                return stringPerson;
 #if DEBUG
-            // Verify we are parsing correctly
+    // Verify we are parsing correctly
             try {
                 var jObject = JObject.Parse(source);
                 var name = (string)jObject["name"];
@@ -91,6 +76,60 @@ namespace Microsoft.NodejsTools.Npm.SPI {
                 Debug.Assert(source == Name);
             }
 #endif
+            return new Person(source);
+        }
+
+        /// <summary>
+        /// Try to create a person object from a json object.
+        /// </summary>
+        /// <param name="source">Json source</param>
+        private static Person TryCreatePersonFromObject(string source) {
+            string name = null;
+            string email = null;
+            string url = null;
+
+            // We parse using a regex because JObject.Parse throws exceptions for malformatted json,
+            // and simply handling them causes performance issues.
+            var matches = ObjectPersonRegex.Matches(source);
+            if (matches.Count >= 1) {
+                foreach (Match match in matches) {
+                    var group = match.Groups["name"];
+                    if (@group.Success) {
+                        name = @group.Value;
+                        continue;
+                    }
+
+                    @group = match.Groups["email"];
+                    if (@group.Success) {
+                        email = @group.Value;
+                        continue;
+                    }
+
+                    @group = match.Groups["url"];
+                    if (@group.Success) {
+                        url = @group.Value;
+                        continue;
+                    }
+                }
+            } else {
+                return null;
+            }
+            return new Person(name, email, url);
+        }
+
+        /// <summary>
+        /// Try to create a person object from a json string.
+        /// 
+        /// TODO: currently does not try to parse the string to extract the email or url.
+        /// </summary>
+        /// <param name="source">Json source</param>
+        private static Person TryCreatePersonFromString(string source) {
+            var matches = StringPersonRegex.Matches(source);
+            if (matches.Count >= 1) {
+                var match = matches[0];
+                return new Person(match.Value);
+            }
+            return null;
         }
 
         [JsonProperty]
