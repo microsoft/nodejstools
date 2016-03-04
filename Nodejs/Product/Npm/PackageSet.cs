d98237cce3e82f9fa8484b43e89eaa928f7e9315
Matt Bierner <matb@microsoft.com>
2016-03-04 13:24:30 -0800

    Add the concept of a PackageSet to manage sets of packages.
    Currently, the UI knows way too much about managing packages, when really all it should do is update itself based on a diff of what packages were added and what packages were removed. Package sets allow creating this diff at the model level.
    
    This will help abstract out some the coupling that currently exists between the UI and the npm packages.

---
+++ Nodejs/Product/Npm/PackageSet.cs
@@ -0,0 +1,68 @@
+﻿//*********************************************************//
+//    Copyright (c) Microsoft. All rights reserved.
+//    
+//    Apache 2.0 License
+//    
+//    You may obtain a copy of the License at
+//    http://www.apache.org/licenses/LICENSE-2.0
+//    
+//    Unless required by applicable law or agreed to in writing, software 
+//    distributed under the License is distributed on an "AS IS" BASIS, 
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or 
+//    implied. See the License for the specific language governing 
+//    permissions and limitations under the License.
+//
+//*********************************************************//
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Microsoft.NodejsTools.Npm {
+    public class PackageSet {
+        public class PackageSetDiff {
+            private IEnumerable<IPackage> _added;
+            private IEnumerable<IPackage> _removed;
+
+            internal PackageSetDiff(IEnumerable<IPackage> added, IEnumerable<IPackage> removed) {
+                _added = added;
+                _removed = removed;
+            }
+
+            public IEnumerable<IPackage> Added { get { return _added; } }
+            public IEnumerable<IPackage> Removed { get { return _removed; } }
+        }
+
+        private List<IPackage> _packages;
+
+        public PackageSet(List<IPackage> packages) {
+            _packages = packages;
+        }
+
+        public IReadOnlyList<IPackage> Packages { get { return _packages; } }
+
+        public PackageSetDiff Diff(PackageSet other) {
+            var added = other.Packages.Except(_packages, new PackageComparer());
+            var removed = _packages.Except(other.Packages, new PackageComparer());
+            return new PackageSetDiff(added, removed);
+        }
+
+        class PackageComparer : EqualityComparer<IPackage> {
+            public override bool Equals(IPackage p1, IPackage p2) {
+                return p1.Name == p2.Name
+                    && p1.Version == p2.Version
+                    && p1.IsBundledDependency == p2.IsBundledDependency
+                    && p1.IsDevDependency == p2.IsDevDependency
+                    && p1.IsListedInParentPackageJson == p2.IsListedInParentPackageJson
+                    && p1.IsMissing == p2.IsMissing
+                    && p1.IsOptionalDependency == p2.IsOptionalDependency;
+            }
+
+            public override int GetHashCode(IPackage obj) {
+                if (obj.Name == null)
+                    return obj.GetHashCode();
+                return obj.Name.GetHashCode();
+            }
+        }
+    }
+}
---
+++ Nodejs/Tests/NpmTests/PackageSetTests.cs
@@ -0,0 +1,119 @@
+﻿//*********************************************************//
+//    Copyright (c) Microsoft. All rights reserved.
+//    
+//    Apache 2.0 License
+//    
+//    You may obtain a copy of the License at
+//    http://www.apache.org/licenses/LICENSE-2.0
+//    
+//    Unless required by applicable law or agreed to in writing, software 
+//    distributed under the License is distributed on an "AS IS" BASIS, 
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or 
+//    implied. See the License for the specific language governing 
+//    permissions and limitations under the License.
+//
+//*********************************************************//
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using TestUtilities;
+using Microsoft.NodejsTools.Npm;
+using System.Linq;
+using Moq;
+
+namespace NodejsTests {
+    [TestClass]
+    public class PackageSetTests {
+        [TestMethod]
+        public void DiffShouldReturnEmptyDiffForEmptyPackageSets() {
+            var package1 = new PackageSet(new List<IPackage>());
+            var package2 = new PackageSet(new List<IPackage>());
+
+            var diff = package1.Diff(package2);
+            Assert.IsFalse(diff.Added.Any());
+            Assert.IsFalse(diff.Removed.Any());
+        }
+
+        [TestMethod]
+        public void DiffShouldReturnAllAsAddedWhenEmpty() {
+            var packageList = new List<IPackage>() {
+                new Mock<IPackage>().Object,
+                new Mock<IPackage>().Object,
+                new Mock<IPackage>().Object
+            };
+
+            var package1 = new PackageSet(new List<IPackage>());
+            var package2 = new PackageSet(packageList);
+
+            var diff = package1.Diff(package2);
+            AssertUtil.ArrayEquals(packageList, diff.Added);
+            Assert.IsFalse(diff.Removed.Any());
+        }
+
+        [TestMethod]
+        public void DiffShouldReturnAllAsRemovedAgainstWhenDiffedAgainstEmpty() {
+            var packageList = new List<IPackage>() {
+                new Mock<IPackage>().Object,
+                new Mock<IPackage>().Object,
+                new Mock<IPackage>().Object
+            };
+
+            var package1 = new PackageSet(packageList);
+            var package2 = new PackageSet(new List<IPackage>());
+
+            var diff = package1.Diff(package2);
+            Assert.IsFalse(diff.Added.Any());
+            AssertUtil.ArrayEquals(packageList, diff.Removed);
+        }
+
+        [TestMethod]
+        public void DiffShouldReturnForPackagesSetsWithSharedPackages() {
+            var p1 = new Mock<IPackage>().Object;
+            var p2 = new Mock<IPackage>().Object;
+            var p3 = new Mock<IPackage>().Object;
+
+            var package1 = new PackageSet(new List<IPackage>() { p1, p2 });
+            var package2 = new PackageSet(new List<IPackage>() { p1, p3 });
+
+            var diff = package1.Diff(package2);
+            AssertUtil.ArrayEquals(new List<IPackage>() { p3 }, diff.Added);
+            AssertUtil.ArrayEquals(new List<IPackage>() { p2 }, diff.Removed);
+        }
+        
+        [TestMethod]
+        public void DiffShouldNotMarkPackagesWithSameValuesAsDifferent() {
+            var ver = new SemverVersion(1, 2, 3);
+            var name = "package";
+
+            var p1 = Mock.Of<IPackage>(x => x.Name == name && x.Version == ver);
+            var p2 = Mock.Of<IPackage>();
+            var newP1 = Mock.Of<IPackage>(x => x.Name == name && x.Version == ver);
+
+            var package1 = new PackageSet(new List<IPackage>() { p1, p2 });
+            var package2 = new PackageSet(new List<IPackage>() { newP1 });
+
+            var diff = package1.Diff(package2);
+            Assert.IsFalse(diff.Added.Any());
+            AssertUtil.ArrayEquals(new List<IPackage>() { p2 }, diff.Removed);
+        }
+
+        [TestMethod]
+        public void DiffShouldMarkPackagesWithSameNameButDifferentVersionsAsDifferent() {
+            var p1Version = new SemverVersion(1, 2, 3);
+            var p2Version = new SemverVersion(1, 2, 4);
+
+            var name = "package";
+
+            var p1 = Mock.Of<IPackage>(x => x.Name == name && x.Version == p1Version);
+            var p2 = Mock.Of<IPackage>();
+            var newP1 = Mock.Of<IPackage>(x => x.Name == name && x.Version == p2Version);
+
+            var package1 = new PackageSet(new List<IPackage>() { p1, p2 });
+            var package2 = new PackageSet(new List<IPackage>() { newP1 });
+
+            var diff = package1.Diff(package2);
+            AssertUtil.ArrayEquals(new List<IPackage>() { newP1 }, diff.Added);
+            AssertUtil.ArrayEquals(new List<IPackage>() { p1, p2 }, diff.Removed);
+        }
+    }
+}
