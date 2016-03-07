11e25bd0f010ba649bd0e5e24c059c2b9a8aed2e
Matt Bierner <matb@microsoft.com>
2016-03-07 15:48:51 -0800

    Target one version of vs specifically

--- Tools/runtests.ps1
+++ Tools/runtests.ps1
@@ -1,24 +1,26 @@
 # Run the basic sets of tests using powershell
-$vsversions = @("12.0", "14.0", "15.0")
+$vsversions = @("15.0", "14.0", "12.0")
 
 # Set of tests to run
 # AzurePublishing.Tests.UI are currently skipped.
 $tests = @("JSAnalysisTests", "Nodejs.Tests.UI", "NodeTests","NpmTests", "ProfilerTests", "SharedProjectTests")
 
 # Try to get the path to the vstest.console test runner
+$selectedVsVersion = ''
 $vstest =  ''
 foreach($vsversion in $vsversions) {
 	$path = "C:\Program Files (x86)\Microsoft Visual Studio $vsversion\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe"
-	if (Test-Path $path) {
+	if (($selectedVsVersion -eq '') -and (Test-Path $path)) {
+		$selectedVsVersion = $vsversion
 		$vstest = $path
 	}
 }
 
-if ($vstest -eq '') {
+if ($selectedVsVersion -eq '') {
 	echo "Could not find valid vstest.console"
 	return
 }
 
-$testFiles = $tests | % { ".\BuildOutput\Release14.0\Tests\$_.dll" }
+$testFiles = $tests | % { ".\BuildOutput\Release$selectedVsVersion\Tests\$_.dll" }
 
 & "$vstest" @args $testFiles
