92b8435ce647ac8fe8993854beb356c360e3ce14
Matt Bierner <matb@microsoft.com>
2016-04-05 12:05:30 -0700

    Addressing comments in #414

--- Nodejs/Tests/Core/Mocks/MockTextBuffer.cs
+++ Nodejs/Tests/Core/Mocks/MockTextBuffer.cs
@@ -1,20 +1,32 @@
-﻿using System.IO;
+﻿//*********************************************************//
+//    Copyright (c) Microsoft. All rights reserved.
+//    
+//    Apache 2.0 License
+//    
+//    You may obtain a copy of the License at
+//    http://www.apache.org/licenses/LICENSE-2.0
+//    
+//    Unless required by applicable law or agreed to in writing, software 
+//    distributed under the License is distributed on an "AS IS" BASIS, 
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or 
+//    implied. See the License for the specific language governing 
+//    permissions and limitations under the License.
+//
+//*********************************************************//
+
+using System.IO;
 using Microsoft.NodejsTools;
 
 namespace NodejsTests.Mocks {
     class MockTextBuffer : TestUtilities.Mocks.MockTextBuffer {
-		public MockTextBuffer(string content) : base(content: content, contentType: NodejsConstants.Nodejs) {
-        }
+        public MockTextBuffer(string content) :
+            base(content: content, contentType: NodejsConstants.Nodejs) { }
+
+        public MockTextBuffer(string content, string contentType, string filename = null) :
+            base(content: content,contentType: contentType, filename: GetRandomFileNameIfNull(filename)) { }
 
         private static string GetRandomFileNameIfNull(string filename) {
-            if (filename == null) {
-                filename = Path.Combine(TestUtilities.TestData.GetTempPath(), Path.GetRandomFileName(), "file.js");
-            }
-            return filename;
-        }
-
-        public MockTextBuffer(string content, string contentType, string filename = null) : base(content: content,
-            contentType: contentType, filename: MockTextBuffer.GetRandomFileNameIfNull(filename)) {
+            return filename ?? Path.Combine(TestUtilities.TestData.GetTempPath(), Path.GetRandomFileName(), "file.js");
         }
     }
 }
