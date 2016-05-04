56a6d35a0176d2da661fbe07bf5db520fdc5d2b3
Matt Bierner <matb@microsoft.com>
2016-05-03 17:16:45 -0700

    Unregister options page callback from NodejsProjectNode

--- Nodejs/Product/Nodejs/Project/NodejsProjectNode.cs
+++ Nodejs/Product/Nodejs/Project/NodejsProjectNode.cs
@@ -1020,6 +1020,7 @@ namespace Microsoft.NodejsTools.Project {
                 NodejsPackage.Instance.IntellisenseOptionsPage.SaveToDiskChanged -= IntellisenseOptionsPageSaveToDiskChanged;
                 NodejsPackage.Instance.IntellisenseOptionsPage.AnalysisLevelChanged -= IntellisenseOptionsPageAnalysisLevelChanged;
                 NodejsPackage.Instance.IntellisenseOptionsPage.AnalysisLogMaximumChanged -= AnalysisLogMaximumChanged;
+                NodejsPackage.Instance.GeneralOptionsPage.ShowBrowserAndNodeLabelsChanged -= ShowBrowserAndNodeLabelsChanged;
 
                 OnDispose?.Invoke(this, EventArgs.Empty);
 
