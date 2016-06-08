c2ad6fb902f2c2a05b607889debd51be71bb9d01
Jamie Cansdale <jcansdale+github@gmail.com>
2016-06-09 00:52:03 +0100

    Factored out findTape and loadTestCases. When loading test script throws, output error and continue loading other scripts. Use strict.

--- Nodejs/Product/Nodejs/TestFrameworks/Tape/tape.js
+++ Nodejs/Product/Nodejs/TestFrameworks/Tape/tape.js
@@ -1,12 +1,11 @@
+"use strict";
+
 var fs = require('fs');
 var path = require('path');
 
 function find_tests(testFileList, discoverResultFile, projectFolder) {
     var test = findTape(projectFolder);
-    if (test === null) {
-        console.error("NTVS_ERROR:", "Couldn't find 'tape' module relative to", projectFolder);
-        return;
-    }
+    if (test === null) return;
     
     var harness = test.getHarness({ exit: false });
     var tests = harness["_tests"];
@@ -14,19 +13,12 @@ function find_tests(testFileList, discoverResultFile, projectFolder) {
     var count = 0;
     var testList = [];
     testFileList.split(';').forEach(function (testFile) {
-
-        var testCases;
-        process.chdir(path.dirname(testFile));
-        try {
-            testCases = require(testFile);
-        } catch (ex) {
-            console.error("NTVS_ERROR:", ex, "in", testFile);
-            return;
-        }
+        var testCases = loadTestCases(testFile);
+        if (testCases === null) return; // continue to next testFile
 
         for (; count < tests.length; count++) {
             var t = tests[count];
-            t._skip = true; // Don't run tests.
+            t._skip = true; // don't run tests
             testList.push({
                 test: t.name,
                 suite: '',
@@ -44,22 +36,13 @@ function find_tests(testFileList, discoverResultFile, projectFolder) {
 module.exports.find_tests = find_tests;
 
 function run_tests(testName, testFile, workingFolder, projectFolder) {
-    var testCases;
-    process.chdir(path.dirname(testFile));
-    try {
-        testCases = require(testFile);
-    } catch (ex) {
-        console.error("NTVS_ERROR:", ex, "in", testFile);
-        return;
-    }
+    var testCases = loadTestCases(testFile);
+    if (testCases === null) return;
+
+    var test = findTape(projectFolder);
+    if (test === null) return;
 
     try {
-        var test = findTape(projectFolder);
-        if (test === null) {
-            console.error("NTVS_ERROR:", "Couldn't find 'tape' module relative to", projectFolder);
-            return;
-        }
-
         var harness = test.getHarness();
         harness.only(testName);
     } catch (ex) {
@@ -69,7 +52,22 @@ function run_tests(testName, testFile, workingFolder, projectFolder) {
 }
 module.exports.run_tests = run_tests;
 
+function loadTestCases(testFile) {
+    try {
+        process.chdir(path.dirname(testFile));
+        return require(testFile);
+    } catch (e) {
+        console.error("NTVS_ERROR:", e, "in", testFile);
+        return null;
+    }
+}
+
 function findTape(projectFolder) {
-    var tapePath = path.join(projectFolder, 'node_modules', 'tape');
-    return require(tapePath);
+    try {
+        var tapePath = path.join(projectFolder, 'node_modules', 'tape');
+        return require(tapePath);
+    } catch (e) {
+        console.error("NTVS_ERROR:", "Couldn't find 'tape' module relative to", projectFolder);
+        return null;
+    }
 }
