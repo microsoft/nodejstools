9c116b5b41e27b5c9a5bacf043d7ae8018c63f20
Matt Bierner <matb@microsoft.com>
2016-08-18 16:38:27 -0700

    Reverting main change

---
+++ Nodejs/Product/Nodejs/ProjectTemplates/StarterExpressApp/_references.js
@@ -0,0 +1,1 @@
+/// <autosync enabled="true" />/// <reference path="modernizr-2.6.2.js" />/// <reference path="jquery-1.10.2.js" />/// <reference path="bootstrap.js" />/// <reference path="respond.js" />
---
+++ Nodejs/Product/Nodejs/ProjectTemplates/TypeScriptAzureStarterExpressApp/index.ts
@@ -0,0 +1,16 @@
+﻿/*
+ * GET home page.
+ */
+import express = require('express');
+
+export function index(req: express.Request, res: express.Response) {
+    res.render('index', { title: 'Express', year: new Date().getFullYear() });
+};
+
+export function about(req: express.Request, res: express.Response) {
+    res.render('about', { title: 'About', year: new Date().getFullYear(), message: 'Your application description page' });
+};
+
+export function contact(req: express.Request, res: express.Response) {
+    res.render('contact', { title: 'Contact', year: new Date().getFullYear(), message: 'Your contact page' });
+};
