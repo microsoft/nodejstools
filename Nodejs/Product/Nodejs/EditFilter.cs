c07d75e6d9d9eb49ac6b998875ba04ac917dea45
Matt Bierner <matb@microsoft.com>
2016-09-12 10:00:51 -0700

    Delete Old Comment Logic
    **Bug**
    We are removing the old node editor. Our custom comment logic is no longer needed. This functionality can only be reached through the interactive window, and the use case for commenting a block of code in the repl is rather limited.
    
    **Fix**
    Remove this logic.

--- Nodejs/Product/Nodejs/EditFilter.cs
+++ Nodejs/Product/Nodejs/EditFilter.cs
@@ -123,18 +123,6 @@ namespace Microsoft.NodejsTools {
                     case VSConstants.VSStd2KCmdID.PASTE:
                         return Paste(ref pguidCmdGroup, nCmdID, nCmdexecopt, pvaIn, pvaOut, out hr);
 
-                    case VSConstants.VSStd2KCmdID.COMMENT_BLOCK:
-                    case VSConstants.VSStd2KCmdID.COMMENTBLOCK:
-                        if (_textView.CommentOrUncommentBlock(comment: true)) {
-                            return VSConstants.S_OK;
-                        }
-                        break;
-                    case VSConstants.VSStd2KCmdID.UNCOMMENT_BLOCK:
-                    case VSConstants.VSStd2KCmdID.UNCOMMENTBLOCK:
-                        if (_textView.CommentOrUncommentBlock(comment: false)) {
-                            return VSConstants.S_OK;
-                        }
-                        break;
                     case VSConstants.VSStd2KCmdID.OUTLN_STOP_HIDING_ALL:
                         tagger = _textView.GetOutliningTagger();
                         if (tagger != null) {
