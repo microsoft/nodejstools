7d8ad2eb9bd12345bed80d74542e23c412ed0ece
Matt Bierner <matb@microsoft.com>
2016-09-14 17:58:41 -0700

    Upgrade TypeScript Express App Template To Express 4 (#1277)
    Moves the existing TypeScriptExpressApp Template to use express 4. This includes:
    
    * Updating to use new/changed apis.
    * Including modern typings files in a way that emulates what `typings` does
    * Adding a bin/www file.
    * Small other fixes.

--- Nodejs/Product/Nodejs/ProjectTemplates/TypeScriptExpressApp/user.ts
+++ Nodejs/Product/Nodejs/ProjectTemplates/TypeScriptExpressApp/user.ts
@@ -2,7 +2,10 @@
  * GET users listing.
  */
 import express = require('express');
+const router = express.Router();
 
-export function list(req: express.Request, res: express.Response) {
+router.get('/', (req: express.Request, res: express.Response) => {
     res.send("respond with a resource");
-};
+});
+
+export default router;
