dd670ce1c2f034ec5a14043ff8f2c17e16d6f2f2
Paul van Brenk <paul.van.brenk@microsoft.com>
2016-12-07 17:12:07 -0800

    Add localization to nodetools
    * Adds locazibility
    * ensures new files are signed
    * Includes new files in deliverables

--- Nodejs/Product/Nodejs/Options/NodejsNpmOptionsControl.Designer.cs
+++ Nodejs/Product/Nodejs/Options/NodejsNpmOptionsControl.Designer.cs
@@ -23,6 +23,7 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(NodejsNpmOptionsControl));
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
             this._showOutputWhenRunningNpm = new System.Windows.Forms.CheckBox();
@@ -41,143 +42,68 @@
             // 
             // groupBox1
             // 
-            this.groupBox1.AutoSize = true;
-            this.groupBox1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            resources.ApplyResources(this.groupBox1, "groupBox1");
             this.groupBox1.Controls.Add(this.tableLayoutPanel3);
-            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.groupBox1.Location = new System.Drawing.Point(6, 3);
-            this.groupBox1.Margin = new System.Windows.Forms.Padding(6, 3, 6, 3);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(376, 42);
-            this.groupBox1.TabIndex = 2;
             this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "npm Command Execution";
             // 
             // tableLayoutPanel3
             // 
-            this.tableLayoutPanel3.AutoSize = true;
-            this.tableLayoutPanel3.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.tableLayoutPanel3.ColumnCount = 1;
-            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            resources.ApplyResources(this.tableLayoutPanel3, "tableLayoutPanel3");
             this.tableLayoutPanel3.Controls.Add(this._showOutputWhenRunningNpm, 0, 0);
-            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Top;
-            this.tableLayoutPanel3.Location = new System.Drawing.Point(3, 16);
-            this.tableLayoutPanel3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.tableLayoutPanel3.Name = "tableLayoutPanel3";
-            this.tableLayoutPanel3.RowCount = 1;
-            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel3.Size = new System.Drawing.Size(370, 23);
-            this.tableLayoutPanel3.TabIndex = 2;
             // 
             // _showOutputWhenRunningNpm
             // 
-            this._showOutputWhenRunningNpm.AutoSize = true;
-            this._showOutputWhenRunningNpm.Location = new System.Drawing.Point(3, 3);
+            resources.ApplyResources(this._showOutputWhenRunningNpm, "_showOutputWhenRunningNpm");
             this._showOutputWhenRunningNpm.Name = "_showOutputWhenRunningNpm";
-            this._showOutputWhenRunningNpm.Size = new System.Drawing.Size(228, 17);
-            this._showOutputWhenRunningNpm.TabIndex = 1;
-            this._showOutputWhenRunningNpm.Text = "Show Output window when executing &npm";
             this._showOutputWhenRunningNpm.UseVisualStyleBackColor = true;
             // 
             // groupBox2
             // 
-            this.groupBox2.AutoSize = true;
-            this.groupBox2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            resources.ApplyResources(this.groupBox2, "groupBox2");
             this.groupBox2.Controls.Add(this.tableLayoutPanel1);
-            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.groupBox2.Location = new System.Drawing.Point(6, 51);
-            this.groupBox2.Margin = new System.Windows.Forms.Padding(6, 3, 6, 3);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(376, 74);
-            this.groupBox2.TabIndex = 3;
             this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "npm Package Catalog";
             // 
             // tableLayoutPanel1
             // 
-            this.tableLayoutPanel1.AutoSize = true;
-            this.tableLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.tableLayoutPanel1.ColumnCount = 3;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 260F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            resources.ApplyResources(this.tableLayoutPanel1, "tableLayoutPanel1");
             this.tableLayoutPanel1.Controls.Add(this.ClearCacheButton, 1, 0);
             this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
             this.tableLayoutPanel1.Controls.Add(this._cacheClearedSuccessfully, 2, 0);
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 16);
-            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 1;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(367, 38);
-            this.tableLayoutPanel1.TabIndex = 0;
             // 
             // ClearCacheButton
             // 
-            this.ClearCacheButton.Anchor = System.Windows.Forms.AnchorStyles.Left;
-            this.ClearCacheButton.Location = new System.Drawing.Point(263, 7);
-            this.ClearCacheButton.Margin = new System.Windows.Forms.Padding(3, 3, 0, 3);
+            resources.ApplyResources(this.ClearCacheButton, "ClearCacheButton");
             this.ClearCacheButton.Name = "ClearCacheButton";
-            this.ClearCacheButton.Size = new System.Drawing.Size(74, 23);
-            this.ClearCacheButton.TabIndex = 0;
-            this.ClearCacheButton.Text = "&Clear Cache";
             this.ClearCacheButton.UseVisualStyleBackColor = true;
             this.ClearCacheButton.Click += new System.EventHandler(this.ClearCacheButton_Click);
             // 
             // label1
             // 
-            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left)));
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(3, 6);
-            this.label1.Margin = new System.Windows.Forms.Padding(3, 6, 3, 6);
+            resources.ApplyResources(this.label1, "label1");
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(249, 26);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Delete data from local Node.js Tools npm package catalog cache";
             // 
             // _cacheClearedSuccessfully
             // 
-            this._cacheClearedSuccessfully.AccessibleDescription = "Cache Cleared Successfully";
-            this._cacheClearedSuccessfully.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left)));
-            this._cacheClearedSuccessfully.AutoSize = true;
-            this._cacheClearedSuccessfully.Font = new System.Drawing.Font("Segoe UI Symbol", 14F, System.Drawing.FontStyle.Bold);
-            this._cacheClearedSuccessfully.Location = new System.Drawing.Point(337, 0);
-            this._cacheClearedSuccessfully.Margin = new System.Windows.Forms.Padding(0, 0, 3, 0);
+            resources.ApplyResources(this._cacheClearedSuccessfully, "_cacheClearedSuccessfully");
             this._cacheClearedSuccessfully.Name = "_cacheClearedSuccessfully";
-            this._cacheClearedSuccessfully.Size = new System.Drawing.Size(27, 38);
-            this._cacheClearedSuccessfully.TabIndex = 2;
-            this._cacheClearedSuccessfully.Text = "✓";
-            this._cacheClearedSuccessfully.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this._cacheClearedSuccessfully.Visible = false;
             // 
             // tableLayoutPanel2
             // 
-            this.tableLayoutPanel2.AutoSize = true;
-            this.tableLayoutPanel2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.tableLayoutPanel2.ColumnCount = 1;
-            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            resources.ApplyResources(this.tableLayoutPanel2, "tableLayoutPanel2");
             this.tableLayoutPanel2.Controls.Add(this.groupBox1, 0, 0);
             this.tableLayoutPanel2.Controls.Add(this.groupBox2, 0, 1);
-            this.tableLayoutPanel2.Location = new System.Drawing.Point(0, -1);
             this.tableLayoutPanel2.Name = "tableLayoutPanel2";
-            this.tableLayoutPanel2.RowCount = 2;
-            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle());
-            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle());
-            this.tableLayoutPanel2.Size = new System.Drawing.Size(388, 128);
-            this.tableLayoutPanel2.TabIndex = 4;
             // 
             // NodejsNpmOptionsControl
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.AutoSize = true;
-            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.Controls.Add(this.tableLayoutPanel2);
-            this.Margin = new System.Windows.Forms.Padding(6, 8, 6, 8);
             this.Name = "NodejsNpmOptionsControl";
-            this.Size = new System.Drawing.Size(391, 130);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.tableLayoutPanel3.ResumeLayout(false);
