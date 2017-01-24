35358291f3bdbc7eb74bb301d23f9c6e83b549c5
Anubha Mathur <anubmat@microsoft.com>
2017-01-23 17:12:38 -0800

    Add JSX, TSX and TSConfig file templates to Node projects

---
+++ Nodejs/Product/Nodejs/Templates/Files/TypeScriptJSX/EmptyTsx.tsx
@@ -0,0 +1,3 @@
+﻿// A '.tsx' file enables JSX support in the TypeScript compiler, 
+// for more information see the following page on the TypeScript wiki:
+// https://github.com/Microsoft/TypeScript/wiki/JSX
