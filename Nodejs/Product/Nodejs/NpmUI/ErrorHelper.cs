8523ef704978677ef41a587abb76fcdc11fabf3c
Paul van Brenk <paul.van.brenk@microsoft.com>
2017-03-15 13:34:45 -0700

    Remove sqllite binary

--- Nodejs/Product/Nodejs/NpmUI/ErrorHelper.cs
+++ Nodejs/Product/Nodejs/NpmUI/ErrorHelper.cs
@@ -1,10 +1,10 @@
 // Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
 using System;
+using System.Globalization;
 using System.Text;
 using System.Windows;
 using Microsoft.NodejsTools.Npm;
-using System.Globalization;
 
 namespace Microsoft.NodejsTools.NpmUI
 {
