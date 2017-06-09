c56d5a6362d962f5069643b6af6743d21b734427
Paul van Brenk <paul.van.brenk@microsoft.com>
2017-06-09 15:42:35 -0700

    Give usefull error when Nodejs exe is corrupt

--- Nodejs/Product/PressAnyKey/PressAnyKey.cs
+++ Nodejs/Product/PressAnyKey/PressAnyKey.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
 using System;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
@@ -19,22 +20,35 @@ namespace Microsoft.NodejsTools.PressAnyKey
             }
 
             Console.Title = args[2];
-            var psi = new ProcessStartInfo(args[2], string.Join(" ", args.Skip(3).Select(arg => ProcessOutput.QuoteSingleArgument(arg))));
-            psi.UseShellExecute = false;
+            var psi = new ProcessStartInfo(args[2], string.Join(" ", args.Skip(3).Select(arg => ProcessOutput.QuoteSingleArgument(arg))))
+            {
+                UseShellExecute = false
+            };
 
-            var proc = Process.Start(psi);
-            File.WriteAllText(args[1], proc.Id.ToString());
-            proc.WaitForExit();
+            var exitCode = 0;
+            try
+            {
+                var proc = Process.Start(psi);
+                File.WriteAllText(args[1], proc.Id.ToString());
+                proc.WaitForExit();
+                exitCode = proc.ExitCode;
+            }
+            catch (Win32Exception exc)
+            {
+                Console.WriteLine($"Failed to start process: '{exc.Message}'.");
+                Console.WriteLine("Probable cause is the Nodejs exe is corrupt, please re-install.");
+                exitCode = -1;
+            }
 
             if (args[0] == "both" ||
-                (proc.ExitCode == 0 && args[0] == "normal") ||
-                (proc.ExitCode != 0 && args[0] == "abnormal"))
+                (exitCode == 0 && args[0] == "normal") ||
+                (exitCode != 0 && args[0] == "abnormal"))
             {
                 Console.Write("Press any key to continue...");
                 Console.ReadKey();
             }
 
-            return proc.ExitCode;
+            return exitCode;
         }
     }
 }
