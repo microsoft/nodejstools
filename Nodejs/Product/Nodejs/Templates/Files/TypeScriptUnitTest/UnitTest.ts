512682f7a94181ffb0e6fa5ca5fdf67fda525f3f
Paul van Brenk <paul.van.brenk@microsoft.com>
2017-08-21 17:22:43 -0700

    Clean-up item templates

--- Nodejs/Product/Nodejs/Templates/Files/TypeScriptUnitTest/UnitTest.ts
+++ Nodejs/Product/Nodejs/Templates/Files/TypeScriptUnitTest/UnitTest.ts
@@ -2,9 +2,9 @@
 
 export function Test1() {
     assert.ok(true, "This shouldn't fail");
-}
+};
 
 export function Test2() {
     assert.ok(1 === 1, "This shouldn't fail");
     assert.ok(false, "This should fail");
-}
+};
