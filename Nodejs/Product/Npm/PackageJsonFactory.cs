3ed53733c75febff71f839e8637094165f0717ea
Paul van Brenk <paul.van.brenk@microsoft.com>
2017-11-27 15:32:45 -0800

    Simplified and PR feedback

--- Nodejs/Product/Npm/PackageJsonFactory.cs
+++ Nodejs/Product/Npm/PackageJsonFactory.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
 using System;
-using System.Globalization;
 using System.IO;
 using System.Threading;
 using Microsoft.NodejsTools.Npm.SPI;
@@ -14,7 +13,7 @@ namespace Microsoft.NodejsTools.Npm
     {
         public static IPackageJson Create(string fullPathToFile)
         {
-            if (string.IsNullOrEmpty(fullPathToFile) && !fullPathToFile.EndsWith("package.json"))
+            if (fullPathToFile == null || !fullPathToFile.EndsWith("package.json", StringComparison.OrdinalIgnoreCase))
             {
                 throw new ArgumentException("Expected full path to 'package.json' file.", nameof(fullPathToFile));
             }
@@ -32,16 +31,33 @@ namespace Microsoft.NodejsTools.Npm
                         using (var fin = new FileStream(fullPathToFile, FileMode.Open, FileAccess.Read, FileShare.Read))
                         using (var reader = new StreamReader(fin))
                         {
-                            return Create(reader);
+                            object source = null;
+
+                            var text = reader.ReadToEnd();
+                            try
+                            {
+                                // JsonConvert and JObject.Parse exhibit slightly different behavior,
+                                // so fall back to JObject.Parse if JsonConvert does not properly deserialize
+                                // the object.
+                                source = JsonConvert.DeserializeObject(text);
+                            }
+                            catch (ArgumentException)
+                            {
+                                source = JObject.Parse(text);
+                            }
+
+                            return source == null ? null : new PackageJson(source);
                         }
                     }
-                    catch (PackageJsonException pje)
+                    catch (Exception exc) when (attempts > 0 && (exc is IOException || exc is UnauthorizedAccessException))
                     {
-                        WrapExceptionAndRethrow(pje);
+                        // only retry on IO related exceptions
                     }
-                    catch (Exception exc) when (exc is IOException || exc is UnauthorizedAccessException)
+                    catch (Exception exc) when (exc is JsonReaderException || exc is JsonSerializationException || exc is FormatException || exc is ArgumentException)
                     {
-                        if (attempts <= 0) { throw; }
+                        // on deserialization exceptions wrap in specific exception which the callers will handle
+                        throw new PackageJsonException($"Error reading '{fullPathToFile}'. The file may be parseable JSON but may contain objects with duplicate properties. " +
+                                    $"The following error occurred: '{exc.Message}'", exc);
                     }
 
                     Thread.Sleep(retryInterval);
@@ -50,60 +66,6 @@ namespace Microsoft.NodejsTools.Npm
             }
 
             return null;
-
-            void WrapExceptionAndRethrow(Exception ex)
-            {
-                throw new PackageJsonException(
-                    string.Format(CultureInfo.CurrentCulture, @"Error reading package.json at '{0}': {1}", fullPathToFile, ex.Message),
-                    ex);
-            }
-        }
-
-        public static IPackageJson Create(TextReader reader)
-        {
-            object source = null;
-            try
-            {
-                var text = reader.ReadToEnd();
-                try
-                {
-                    // JsonConvert and JObject.Parse exhibit slightly different behavior,
-                    // so fall back to JObject.Parse if JsonConvert does not properly deserialize
-                    // the object.
-                    source = JsonConvert.DeserializeObject(text);
-                }
-                catch (ArgumentException)
-                {
-                    source = JObject.Parse(text);
-                }
-            }
-            catch (Exception jre) when (jre is JsonReaderException || jre is JsonSerializationException || jre is FormatException)
-            {
-                WrapExceptionAndRethrow(jre);
-            }
-            catch (ArgumentException ae)
-            {
-                throw new PackageJsonException(
-                    string.Format(CultureInfo.CurrentCulture, @"Error reading package.json. The file may be parseable JSON but may contain objects with duplicate properties.
-
-The following error occurred:
-
-{0}", ae.Message),
-                    ae);
-            }
-
-            return source == null ? null : new PackageJson(source);
-
-            void WrapExceptionAndRethrow(Exception ex)
-            {
-                throw new PackageJsonException(
-                    string.Format(CultureInfo.CurrentCulture, @"Unable to read package.json. Please ensure the file is valid JSON.
-
-Reading failed because the following error occurred:
-
-{0}", ex.Message),
-                    ex);
-            }
         }
     }
 }
