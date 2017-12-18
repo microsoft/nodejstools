75fec95516440ac442d24c9868bcb5d4d80f1aaf
Paul van Brenk <paul.van.brenk@microsoft.com>
2017-12-18 11:27:52 -0800

    Anycode support for debugging package.json

---
+++ Nodejs/Product/Nodejs/TypeScriptHelpers/TsConfigJson.cs
@@ -0,0 +1,38 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System;
+using Newtonsoft.Json.Linq;
+
+namespace Microsoft.NodejsTools.TypeScript
+{
+    internal class TsConfigJson
+    {
+        public readonly bool NoEmit;
+
+        public readonly string OutFile;
+
+        public readonly string OutDir;
+
+        public readonly string RootDir;
+
+        public TsConfigJson(dynamic package)
+        {
+            var compilerOptions = package["compilerOptions"] as JObject;
+
+            this.NoEmit = this.SetNoEmit(compilerOptions);
+            this.OutFile = compilerOptions["outFile"]?.ToString();
+            this.OutDir = compilerOptions["outDir"]?.ToString();
+            this.RootDir = compilerOptions["rootDir"]?.ToString();
+        }
+
+        private bool SetNoEmit(JObject compilerOptions)
+        {
+            return bool.TryParse(compilerOptions["noEmit"]?.ToString(), out var result) && result;
+        }
+
+        public static bool IsValidFile(string filePath)
+        {
+            return filePath?.EndsWith("tsconfig.json", StringComparison.OrdinalIgnoreCase) == true;
+        }
+    }
+}
---
+++ Nodejs/Product/Nodejs/TypeScriptHelpers/TypeScriptCompilerLocator.cs
@@ -0,0 +1,65 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Microsoft.NodejsTools.TypeScript
+{
+    // Copied from VS/LanguageService/TypeScriptLanguageService/Versions/LanguageServiceVersionLocator.cs
+    // in the TypeScript repo. 
+    // TODO: figure out a better way to share functionality between the TypeScript repo and our components
+
+    internal static class TypeScriptCompilerLocator
+    {
+        private static Lazy<IReadOnlyDictionary<Version, string>> orderedAvailableVersions = new Lazy<IReadOnlyDictionary<Version, string>>(() =>
+        {
+            return GetAvailableTypeScriptVersionsFromDisk();
+        });
+
+        public static string GetDefaultVersion()
+        {
+            var latest = orderedAvailableVersions.Value.Keys.Max();
+
+            return orderedAvailableVersions.Value[latest];
+        }
+
+        public static bool TryGetPreferredVersion(Version version, out string path)
+        {
+            return orderedAvailableVersions.Value.TryGetValue(version, out path);
+        }
+
+        private static IReadOnlyDictionary<Version, string> GetAvailableTypeScriptVersionsFromDisk()
+        {
+            var directories = Directory.GetDirectories(GetSdkRoot());
+
+            var dictionary = new Dictionary<Version, string>();
+            foreach (var dir in directories)
+            {
+                if (Version.TryParse(Path.GetFileName(dir), out Version version))
+                {
+                    dictionary.Add(version, dir);
+                }
+            }
+
+            return dictionary;
+        }
+
+        private static string GetSdkRoot()
+        {
+            return Path.Combine(GenerateProgramFiles32(), @"Microsoft SDKs\TypeScript");
+        }
+
+        private static string GenerateProgramFiles32()
+        {
+            var programFilesX86 = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86);
+            if (string.IsNullOrEmpty(programFilesX86))
+            {
+                programFilesX86 = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles);
+            }
+
+            return programFilesX86;
+        }
+    }
+}
