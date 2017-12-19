3d5f0259d28310e04258240625dd7bed7d95b74f
Paul van Brenk <paul.van.brenk@microsoft.com>
2017-12-19 15:01:27 -0800

    Delete LambdaConverter for WPF
    Only used in a single location, and caused file locks during build

---
+++ Nodejs/Product/Nodejs/NpmUI/FilterStateVisibilityConverter.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Microsoft.NodejsTools.NpmUI
+{
+    public sealed class FilterStateVisibilityConverter : IValueConverter
+    {
+        object IValueConverter.Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string state && parameter is string expected)
+            {
+                return StringComparer.OrdinalIgnoreCase.Equals(state, expected) ? Visibility.Visible : Visibility.Hidden;
+            }
+
+            throw new InvalidCastException("Wrong input type.");
+        }
+
+        object IValueConverter.ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
