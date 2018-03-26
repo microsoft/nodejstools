2ee4ff15e0a642a59dd7fa36be2b97bbd6a51bf3
Paul van Brenk <paul.van.brenk@microsoft.com>
2018-03-26 16:48:35 -0700

    Improvements for test assembly locating during debugging

--- Nodejs/Product/Nodejs/TestFrameworks/TestFrameworkDirectories.cs
+++ Nodejs/Product/Nodejs/TestFrameworks/TestFrameworkDirectories.cs
@@ -45,9 +45,9 @@ namespace Microsoft.NodejsTools.TestFrameworks
         {
             // This class is used in 2 different assemblies, installed in 2 locations:
             //
-            // "C:\Program Files (x86)\Microsoft Visual Studio\Preview\Enterprise\Common7\IDE\Extensions\Microsoft\NodeJsTools\NodeJsTools\Microsoft.NodejsTools.dll"
+            // "<VSROOT>\Common7\IDE\Extensions\Microsoft\NodeJsTools\NodeJsTools\Microsoft.NodejsTools.dll"
             // and
-            // "C:\Program Files (x86)\Microsoft Visual Studio\Preview\Enterprise\Common7\IDE\Extensions\Microsoft\NodeJsTools\TestAdapter\Microsoft.NodejsTools.TestAdapter.dll"
+            // "<VSROOT>\Common7\IDE\Extensions\Microsoft\NodeJsTools\TestAdapter\Microsoft.NodejsTools.TestAdapter.dll"
 
             string testAdapterAssemblyFolder;
 
@@ -61,10 +61,24 @@ namespace Microsoft.NodejsTools.TestFrameworks
             {
                 var NodeJsToolsFolder = Path.GetDirectoryName(currentAssembly.Location);
                 testAdapterAssemblyFolder = Path.Combine(Path.GetDirectoryName(NodeJsToolsFolder), TestAdapterFolderName);
+
+#if DEBUG
+                // when debugging the experimental instance the folders are slightly different
+                // the Test frameworks are here: %localappdata%\Microsoft\VisualStudio\15.0_2de0f20fExp\Extensions\Microsoft\Node.js Test Adapter\42.42.42.42\TestFrameworks
+                // Node JS Tools folder is here: %localappdata%\Microsoft\VisualStudio\15.0_2de0f20fExp\Extensions\Microsoft\Node.js Tools\42.42.42.42
+                if (!Directory.Exists(Path.Combine(testAdapterAssemblyFolder, TestFrameworksFolderName)))
+                {
+                    var version = Path.GetFileName(NodeJsToolsFolder);
+                    var microsoftRoot = NodeJsToolsFolder.Substring(0, NodeJsToolsFolder.Length - version.Length - "Node.js Tools".Length);
+
+                    return Path.Combine(microsoftRoot, "Node.js Test Adapter", version, TestFrameworksFolderName);
+                }
+#endif
+
             }
             else
             {
-                throw new InvalidOperationException($"Unable to find '{TestFrameworksFolderName}' folder.");
+                throw new InvalidOperationException($"Called from unexepected assembly: '{currentAssembly.FullName}'.");
             }
 
             return Path.Combine(testAdapterAssemblyFolder, TestFrameworksFolderName);
