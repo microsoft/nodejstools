ea45bb75e42b21bb1a28fddb075f116829cb847f
Paul van Brenk <paul.van.brenk@microsoft.com>
2018-04-17 11:15:20 -0700

    Clean usings

--- Nodejs/Product/Nodejs/SharedProject/OutputWindowWrapper.cs
+++ Nodejs/Product/Nodejs/SharedProject/OutputWindowWrapper.cs
@@ -2,7 +2,6 @@
 
 using System;
 using System.Collections.Concurrent;
-using System.Collections.Generic;
 using System.ComponentModel.Composition;
 using System.Diagnostics;
 using Microsoft.VisualStudio;
