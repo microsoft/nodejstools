76acc9fbb47ffbe860aaf571dac93b4f2d6f563d
Armando Aguirre <armando.aguirre@microsoft.com>
2018-06-05 14:13:43 -0700

    Vue - Replaced tabs for spaces, fixed eslint warnings.

--- Nodejs/Product/Nodejs/ProjectTemplates/TypeScriptVuejsApp/App.vue
+++ Nodejs/Product/Nodejs/ProjectTemplates/TypeScriptVuejsApp/App.vue
@@ -1,19 +1,19 @@
 <template>
-	<div id="app">
-		<Home msg="Hello world!" />
-	</div>
+    <div id="app">
+        <Home msg="Hello world!" />
+    </div>
 </template>
 
 <script lang="ts">
-	import { Component, Vue } from 'vue-property-decorator';
-	import Home from './components/Home.vue';
+    import { Component, Vue } from 'vue-property-decorator';
+    import Home from './components/Home.vue';
 
-	@Component({
-		components: {
-			Home
-		},
-	})
-	export default class App extends Vue { }
+    @Component({
+        components: {
+            Home
+        }
+    })
+    export default class App extends Vue { }
 </script>
 
 <style>
