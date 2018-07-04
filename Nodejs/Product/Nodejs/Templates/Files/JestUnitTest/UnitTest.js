2c420593a19780e66b1ee77ae234c948e98f61af
Armando Aguirre <armando.aguirre@microsoft.com>
2018-07-03 17:51:46 -0700

    Added Jest Unit Test template

---
+++ Nodejs/Product/Nodejs/Templates/Files/JestUnitTest/UnitTest.js
@@ -0,0 +1,10 @@
+﻿describe('Test Suite 1', function () {
+    it("Test 1 - This shouldn't fail", function () {
+        expect(true).toBeTruthy();
+    });
+
+    it('Test 2 - This should fail', function () {
+        expect(1 === 1).toBeTruthy(); // "This shouldn't fail"
+        expect(false).toBeTruthy();
+    });
+});
