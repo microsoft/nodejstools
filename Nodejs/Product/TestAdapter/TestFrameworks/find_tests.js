6240ddd5a1d3b2d3dd70989eabe4752f08a55008
Luke Graham <lugraham@microsoft.com>
2018-09-06 10:05:20 -0700

    Fixing path length exception.

--- Nodejs/Product/TestAdapter/TestFrameworks/find_tests.js
+++ Nodejs/Product/TestAdapter/TestFrameworks/find_tests.js
@@ -1,3 +1,4 @@
+var fs = require("fs");
 var framework;
 try {
     framework = require('./' + process.argv[2] + '/' + process.argv[2] + '.js');
@@ -6,7 +7,19 @@ try {
     process.exit(1);
 }
 try {
-    framework.find_tests(process.argv[3], process.argv[4], process.argv[5]);
+    var testFilesListInputFile = process.argv[3];
+    if (!fs.existsSync(testFilesListInputFile)) {
+        throw new Error("testFilesListInputFile '" + testFilesListInputFile + "' does not exist.");
+    }
+
+    var testFilesList = fs.readFileSync(testFilesListInputFile, "utf-8");
+
+    // strip the BOM in case of UTF-8
+    if (testFilesList.charCodeAt(0) === 0xFEFF) {
+        testFilesList = testFilesList.slice(1);
+    }
+
+    framework.find_tests(testFilesList, process.argv[4], process.argv[5]);
 } catch (exception) {
     console.log("NTVS_ERROR:TestFramework (" + process.argv[2] + ") threw an exception processing (" + process.argv[3] + "), " + exception);
     process.exit(1);
