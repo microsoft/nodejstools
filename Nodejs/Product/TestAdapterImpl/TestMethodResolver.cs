cc558fafbd4c72c6df094207e0f0e00ba7d86e6e
Ben Lichtman <belichtm@microsoft.com>
2020-06-15 16:46:28 -0700

    Remove some unused code

--- Nodejs/Product/TestAdapterImpl/TestMethodResolver.cs
+++ Nodejs/Product/TestAdapterImpl/TestMethodResolver.cs
@@ -2,12 +2,8 @@
 
 using System;
 using System.ComponentModel.Composition;
-using System.Runtime.InteropServices;
-using Microsoft.VisualStudio;
 using Microsoft.VisualStudio.Shell;
-using Microsoft.VisualStudio.Shell.Interop;
 using Microsoft.VisualStudio.TestWindow.Extensibility;
-using MSBuild = Microsoft.Build.Evaluation;
 
 namespace Microsoft.NodejsTools.TestAdapter
 {
@@ -17,68 +13,21 @@ namespace Microsoft.NodejsTools.TestAdapter
         private readonly IServiceProvider serviceProvider;
         private readonly TestContainerDiscoverer discoverer;
 
-        #region ITestMethodResolver Members
-
         [ImportingConstructor]
-        public TestMethodResolver([Import(typeof(SVsServiceProvider))]IServiceProvider serviceProvider,
+        public TestMethodResolver(
+            [Import(typeof(SVsServiceProvider))]IServiceProvider serviceProvider,
             [Import]TestContainerDiscoverer discoverer)
         {
             this.serviceProvider = serviceProvider;
             this.discoverer = discoverer;
         }
 
-        public Uri ExecutorUri=> NodejsConstants.ExecutorUri;
+        public Uri ExecutorUri => NodejsConstants.ExecutorUri;
 
         public string GetCurrentTest(string filePath, int line, int lineCharOffset)
         {
-            // TODO: finish implementing this method
+            // TODO: finish implementing this method with specified imports.
             return null;
-
-            var project = this.PathToProject(filePath);
-            if (project != null && this.discoverer.IsProjectKnown(project))
-            {
-                var buildEngine = new MSBuild.ProjectCollection();
-                if (project.TryGetProjectPath(out var projectPath))
-                {
-                    var proj = buildEngine.LoadProject(projectPath);
-
-                    //TODO - Find the matching function
-                    /*
-                    Need identify which method is executing.
-                     */
-                }
-            }
         }
-
-        private IVsProject PathToProject(string filePath)
-        {
-            var rdt = (IVsRunningDocumentTable)this.serviceProvider.GetService(typeof(SVsRunningDocumentTable));
-            IVsHierarchy hierarchy;
-
-            var docData = IntPtr.Zero;
-            try
-            {
-                var hr = rdt.FindAndLockDocument(
-                    (uint)_VSRDTFLAGS.RDT_NoLock,
-                    filePath,
-                    out hierarchy,
-                    out _,
-                    out docData,
-                    out _);
-                ErrorHandler.ThrowOnFailure(hr);
-            }
-            finally
-            {
-                if (docData != IntPtr.Zero)
-                {
-                    Marshal.Release(docData);
-                    docData = IntPtr.Zero;
-                }
-            }
-
-            return hierarchy as IVsProject;
-        }
-
-        #endregion
     }
 }
