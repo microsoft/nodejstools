849902531f5882f81b17b9598808b3adc006cae9
Armando Aguirre <armando.aguirre@microsoft.com>
2021-11-17 20:46:41 -0800

    Updated typescript version and Vue 3

--- Nodejs/Product/Nodejs/ProjectTemplates/TypeScriptVuejsApp/App.vue
+++ Nodejs/Product/Nodejs/ProjectTemplates/TypeScriptVuejsApp/App.vue
@@ -1,20 +1,27 @@
 <template>
-    <div id="app">
-        <Home msg="Hello world!" />
-    </div>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
 </template>
 
 <script lang="ts">
-    import { Component, Vue } from 'vue-property-decorator';
-    import Home from './components/Home.vue';
+import { Options, Vue } from 'vue-class-component';
+import HelloWorld from './components/HelloWorld.vue';
 
-    @Component({
-        components: {
-            Home
-        }
-    })
-    export default class App extends Vue { }
+@Options({
+  components: {
+    HelloWorld,
+  },
+})
+export default class App extends Vue {}
 </script>
 
 <style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
--- Nodejs/Product/Nodejs/ProjectTemplates/TypeScriptVuejsApp/README.md
+++ Nodejs/Product/Nodejs/ProjectTemplates/TypeScriptVuejsApp/README.md
@@ -1,3 +1,19 @@
-w# $projectname$
+# $projectname$
 
+## Project setup
+```
+npm install
+```
 
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
