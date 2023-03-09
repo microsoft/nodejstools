253fa3be744424958102e32d0a4b478b75d99868
Shennie Yao <shennie.yao@microsoft.com>
2023-03-08 16:39:06 -0800

    intermediary commit

--- Nodejs/Product/Nodejs/Commands/MigrateToJspsCommand.cs
+++ Nodejs/Product/Nodejs/Commands/MigrateToJspsCommand.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Threading;
 using Microsoft.VisualStudioTools;
 
 namespace Microsoft.NodejsTools.Commands
@@ -13,7 +15,10 @@ namespace Microsoft.NodejsTools.Commands
 
         public override void DoCommand(object sender, EventArgs args)
         {
-            
+            ThreadHelper.JoinableTaskFactory.RunAsync(async delegate
+            {
+                //Package.GetGlobalService(SVsServiceProvider)
+            });
         }
     }
 }
