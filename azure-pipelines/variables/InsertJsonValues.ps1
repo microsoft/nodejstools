711f6ed2eca6270d2180151d6240b36246065c8e
lucygramley <lucygramley@microsoft.com>
2025-10-17 11:05:16 -0700

    update insertjsonvals path

--- azure-pipelines/variables/InsertJsonValues.ps1
+++ azure-pipelines/variables/InsertJsonValues.ps1
@@ -1,13 +1,15 @@
 $vstsDropNames = & "$PSScriptRoot\VstsDropNames.ps1"
 
-$BasePath = "$PSScriptRoot\..\..\Nodejs\Setup\NodejsTools.vsmanproj"
+# $BasePath = "$PSScriptRoot\..\..\Nodejs\Setup\NodejsTools.vsmanproj"
+$BasePath = "$PSScriptRoot\..\..\BuildOutput\Release\Setup"
 
 if (Test-Path $BasePath) {
     $vsmanFiles = @()
     Get-ChildItem $BasePath *.vsman -Recurse -File |% {
         $version = (Get-Content $_.FullName | ConvertFrom-Json).info.buildVersion
         $fn = $_.Name
-        $vsmanFiles += "NodeJsTools.vsman=https://vsdrop.corp.microsoft.com/file/v1/$vstsDropNames;$fn"
+        $vsmanFiles += "$fn=https://vsdrop.corp.microsoft.com/file/v1/$vstsDropNames;$fn"
+        # $vsmanFiles += "NodeJsTools.vsman=https://vsdrop.corp.microsoft.com/file/v1/$vstsDropNames;$fn"
                         #NodejsTools.vsman=https://vsdrop.corp.microsoft.com/file/v1/Products/DevDiv/microsoft/nodejstools/$(Build.SourceBranchName)/$(Build.BuildNumber);NodejsTools.vsman
     }
 
